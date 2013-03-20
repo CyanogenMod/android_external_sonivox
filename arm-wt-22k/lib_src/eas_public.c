481293b7e5c56729a1793489aebccdc95e7494b2
Amit Shekhar <ashekhar@codeaurora.org>
2013-03-20 14:51:24 -0700

    audio: Miscellaneous fixes in external/sonivox
    Support for mxmf file playback
    
    -mxmf files couldn't be played.
    -Added a condition check, in DLSParser(), to warn user if artCount is 0 and
     to use default articulations.
    
    CRs-Fixed: 465437
    Change-Id: I42b1634c65e3779ee7924913cc17020bde9cc38d
    
    midi decoder lib state is not updated on midiplayer stop
    
     - On EAS_Pause() STREAM_FLAGS_PAUSE is added to streamFlags, but
       EAS_State() returns SUCCESS before checking for streamFlags and
       updating midiplayers state.
     - Fix is to check for streamFlags for STREAM_FLAGS_PAUSE and update
       midiplayers state before returning SUCCESS.
    
    CRs-Fixed: 633774
    Change-Id: Ie8845633f27f05c4917045dcf7cc2b3219c13ef6

--- arm-wt-22k/lib_src/eas_public.c
+++ arm-wt-22k/lib_src/eas_public.c
@@ -1662,10 +1662,6 @@ EAS_PUBLIC EAS_RESULT EAS_State (EAS_DATA_HANDLE pEASData, EAS_HANDLE pStream, E
     if (pStream->repeatCount && (*pState == EAS_STATE_STOPPED))
         *pState = EAS_STATE_PLAY;
 
-    /* if we're not paused or pausing, we don't need to hide state from host */
-    if (*pState != EAS_STATE_PAUSED && *pState != EAS_STATE_PAUSING)
-        return EAS_SUCCESS;
-
     /* if stream is about to be paused, report it as paused */
     if (pStream->streamFlags & STREAM_FLAGS_PAUSE)
     {
@@ -1674,6 +1670,10 @@ EAS_PUBLIC EAS_RESULT EAS_State (EAS_DATA_HANDLE pEASData, EAS_HANDLE pStream, E
         else
             *pState = EAS_STATE_PAUSING;
     }
+
+    /* if we're not paused or pausing, we don't need to hide state from host */
+    if (*pState != EAS_STATE_PAUSED && *pState != EAS_STATE_PAUSING)
+        return EAS_SUCCESS;
 
     /* if stream is about to resume, report it as playing */
     if (pStream->streamFlags & STREAM_FLAGS_RESUME)
